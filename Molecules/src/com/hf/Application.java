ac28191744500c7f40e04030aa5218650364cf6d
Piotr Futymski <piotr.futymski@wp.pl>
2021-03-02 17:31:23 +0100

    project config

---
+++ Molecules/src/com/hf/Application.java
@@ -0,0 +1,11 @@
+package com.hf;
+
+public class Application {
+
+    public static void main(String[] args){
+        MainFrame frame = new MainFrame("Test");
+        frame.setSize(1024,768);
+        frame.setVisible(true);
+    }
+
+}
---
+++ Molecules/src/com/hf/MainFrame.java
@@ -0,0 +1,11 @@
+package com.hf;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(String name){
+        super(name);
+    }
+}
